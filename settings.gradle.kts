026bfb88e65fdd502d9060083f414e54d805f192
Clydeenke <clydeenke@users.noreply.github.com>
2026-08-05 20:00:49 +0800

    fix: switch to Aliyun maven mirrors + set bundled JDK for daemon

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,10 +1,14 @@
 pluginManagement {
     repositories {
+        maven { url = uri("https://maven.aliyun.com/repository/gradle-plugin/") }
+        maven { url = uri("https://maven.aliyun.com/repository/public/") }
+        maven { url = uri("https://maven.aliyun.com/repository/google/") }
         gradlePluginPortal()
         google()
         mavenCentral()
     }
 }
+
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
 }
@@ -12,9 +16,12 @@ plugins {
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
+        maven { url = uri("https://maven.aliyun.com/repository/google/") }
+        maven { url = uri("https://maven.aliyun.com/repository/public/") }
+        maven { url = uri("https://maven.aliyun.com/repository/gradle-plugin/") }
+        maven { url = uri("https://api.xposed.info/") }
         google()
         mavenCentral()
-        maven { url = uri("https://api.xposed.info/") }
     }
 }
 
